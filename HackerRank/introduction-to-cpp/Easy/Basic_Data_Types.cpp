be827dd76875d7ed1e88238f35a55c4e6629fc92
Ismail Hossain <hyethereitsfahim@gmail.com>
2024-12-27 23:52:47 +0600

    Basic_Data_Types.cpp solution for C++ introduction

---
+++ HackerRank/introduction-to-cpp/Easy/Basic_Data_Types.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // write your code here
+    int a;
+    long long int b;
+    char ch;
+    float d;
+    double dd;
+    cin >> a >> b >> ch >> d >> dd;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << ch << endl;
+    cout << fixed << setprecision(3) << d << endl;
+    cout << fixed << setprecision(9) << dd << endl;
+
+    return 0;
+}
