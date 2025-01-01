491d4760359dcd4645b77e9a8925a0616262e734
Ismail Hossain <hyethereitsfahim@gmail.com>
2025-01-01 11:55:35 +0600

    basic C++ program to read and output user details

---
+++ HackerRank/introduction-to-cpp/Easy/Structs.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int age, standard;
+    string first_name, last_name;
+
+    cin >> age >> first_name >> last_name >> standard;
+    cout << age << " " << first_name << " " << last_name << " " << standard << endl;
+
+    return 0;
+}
